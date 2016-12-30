1d9bd8c81bc64bf8dc868f85651855b6ee8b2d26
Dave Nestoff <d.nestoff@gmail.com>
2016-12-29 19:32:38 -0600

    Add meetup request unit test file with setup/tearDown methods

---
+++ meetup_request_test.php
@@ -0,0 +1,43 @@
+<?php
+
+require('meetup_request.php');
+ 
+class MeetupRequestTests extends PHPUnit_Framework_TestCase
+{
+  
+  private $meetup_request;
+
+  protected function setUp()
+  {
+    $this->meetup_request = new MeetupRequest();
+  }
+
+  protected function tearDown()
+  {
+    $this->meetup_request = NULL;
+  }
+
+  public function addDataProvider() {
+    return array(
+      array(1, 2, 3),
+      array(0, 0, 0),
+      array(-1, -1, -2),
+    );
+  }
+
+  /**
+     * @dataProvider addDataProvider
+     */
+
+  public function testeventSearch($a, $b, $expected)
+  {
+    $result = $this->meetup_request->add($a, $b);
+    $this->assertEquals($expected, $result);
+  }
+
+  public function testgroupSearch()
+  {
+
+  }
+ 
+}
