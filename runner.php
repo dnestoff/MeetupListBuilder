a1262beadf3b675a95958c1e6cbba24ddfac50b9
Dave Nestoff <d.nestoff@gmail.com>
2017-01-03 13:18:55 -0600

    Update error handling message for incorrect input statements

--- runner.php
+++ runner.php
@@ -6,7 +6,7 @@ if (isset($argv[1]) && strlen($argv[2]) == 5) {
   $controller = new ListController($argv[1], $argv[2]);
   echo $controller->run();  
 } else {
-  echo "\n\n You haven't entered enough information, please enter your desired search type (<event> OR <group>) and a five-digit <zip code>\n\n";
+  echo "\n\n You haven't entered enough information, please enter your desired command (search type and zipcode):\n\n 'php runner.php <event>/<group> <zip code>'\n\n";
 }
 
 
