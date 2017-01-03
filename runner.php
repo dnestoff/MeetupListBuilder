14e2bd98b4e5a46e89e9bc3f1e75be85820837ef
Dave Nestoff <d.nestoff@gmail.com>
2017-01-03 17:46:17 -0600

    Add additional case to handle incorrectly spelled search types in runner

--- runner.php
+++ runner.php
@@ -2,11 +2,13 @@
 
 require('list_controller.php');
 
-if (isset($argv[1]) && strlen($argv[2]) == 5) {
+if (($argv[1] == 'event' || $argv[1] == 'group') && strlen($argv[2]) == 5) {
   $controller = new ListController($argv[1], $argv[2]);
-  echo $controller->run();  
+  echo $controller->run();
+} else if (isset($argv[1]) && strlen($argv[2]) == 5) {
+  echo "Not sure we caught that correctly. Did you mean 'event' or 'group' when you typed '" . $argv[1] . "'?\n\n";
 } else {
-  echo "\n\n You haven't entered enough information, please enter your desired command (search type and zipcode):\n\n 'php runner.php <event>/<group> <zip code>'\n\n";
+  echo "Oops, something went wrong...\n\nPlease re-enter your desired search type and five-digit zip code:\n\n 'php runner.php <event>/<group> <zip code>'\n\n";
 }
 
 
