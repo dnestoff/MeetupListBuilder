3ec756a6826acdde3e3de35410f89eb56668427a
Dave Nestoff <d.nestoff@gmail.com>
2017-01-03 17:51:03 -0600

    Refactor code

--- runner.php
+++ runner.php
@@ -2,11 +2,15 @@
 
 require('list_controller.php');
 
-if (($argv[1] == 'event' || $argv[1] == 'group') && strlen($argv[2]) == 5) {
-  $controller = new ListController($argv[1], $argv[2]);
+$search_type = $argv[1];
+$zip = $argv[2];
+
+// validate ARGV and create ListController
+if (($search_type == 'event' || $search_type == 'group') && strlen($zip) == 5) {
+  $controller = new ListController($search_type, $zip);
   echo $controller->run();
-} else if (isset($argv[1]) && strlen($argv[2]) == 5) {
-  echo "Not sure we caught that correctly. Did you mean 'event' or 'group' when you typed '" . $argv[1] . "'?\n\n";
+} else if (isset($search_type) && strlen($zip) == 5) {
+  echo "Not sure we caught that correctly. Did you mean 'event' or 'group' when you typed '" . $search_type . "'?\n\n";
 } else {
   echo "Oops, something went wrong...\n\nPlease re-enter your desired search type and five-digit zip code:\n\n 'php runner.php <event>/<group> <zip code>'\n\n";
 }
