7fa25590cf675aaffec08bf988af5fe4d530918d
Dave Nestoff <d.nestoff@gmail.com>
2017-01-03 20:39:18 -0600

    Add event and group data as meetup list test attributes

--- tests/ListControllerTest.php
+++ tests/ListControllerTest.php
@@ -6,6 +6,24 @@ class ListControllerTests extends PHPUnit_Framework_TestCase
 {
   
   private $list_controller;
+  private $event_data = array(
+      array('name' => "Let's get out and get moving!",
+      'time' => 1483552800000,
+      'utc_offset' => -25200000,
+      'group' => array('created' => 1428507043000,
+                  'name' => "Front Range Mind & Body Fitness"),
+      'link' => "https://www.meetup.com/Front-Range-Mind-Body-Fitness-Meetup/events/236488997/",
+      'description' => "<p>Walking is the super food of movement, but like any super food the way you consume it matters."));
+  private $group_data = array(
+      array(
+      'name' => "Women's Development Council (WDC)",
+      'link' => "https://www.meetup.com/womensdevelopmentcouncil/",
+      'description' => "<p>Develop, organize and promote activities that educate, encourage, support and enhance the development of women in business.",
+      'organizer' => array( 'id' => 26790202,
+            'name' => "Jennifer McRae",
+            'bio' => "I began my career in travel in 1988 after receiving a degree in Travel."),
+      'category' => array('id' => 2,
+             'name' => "Career & Business")));
 
   protected function setUp()
   {
@@ -34,20 +52,11 @@ class ListControllerTests extends PHPUnit_Framework_TestCase
       ->setMethods(array('getData'))
       ->getMock();
 
-    $event_data = array(
-      array('name' => "Let's get out and get moving!",
-      'time' => 1483552800000,
-      'utc_offset' => -25200000,
-      'group' => array('created' => 1428507043000,
-                  'name' => "Front Range Mind & Body Fitness"),
-      'link' => "https://www.meetup.com/Front-Range-Mind-Body-Fitness-Meetup/events/236488997/",
-      'description' => "<p>Walking is the super food of movement, but like any super food the way you consume it matters."));
-
     $expected = "\nName: Let's get out and get moving!\nGroup name: Front Range Mind & Body Fitness\nTime: 01-04-2017 @ 11:00\nUrl: https://www.meetup.com/Front-Range-Mind-Body-Fitness-Meetup/events/236488997/\n";
 
     $controller->expects($this->once())
       ->method('getData')
-      ->will($this->returnValue($event_data));
+      ->will($this->returnValue($this->event_data));
 
     $result = $controller->run();
     $this->assertEquals($expected, $result);
@@ -72,22 +81,11 @@ class ListControllerTests extends PHPUnit_Framework_TestCase
       ->setMethods(array('getData'))
       ->getMock();
 
-    $groups_data = array(
-      array(
-      'name' => "Women's Development Council (WDC)",
-      'link' => "https://www.meetup.com/womensdevelopmentcouncil/",
-      'description' => "<p>Develop, organize and promote activities that educate, encourage, support and enhance the development of women in business.",
-      'organizer' => array( 'id' => 26790202,
-            'name' => "Jennifer McRae",
-            'bio' => "I began my career in travel in 1988 after receiving a degree in Travel."),
-      'category' => array('id' => 2,
-             'name' => "Career & Business")));
-
     $expected = "\nName: Women's Development Council (WDC)\nOrganizer name: Jennifer McRae\nCategory: Career & Business\nUrl: https://www.meetup.com/womensdevelopmentcouncil/\n";
 
     $controller->expects($this->once())
       ->method('getData')
-      ->will($this->returnValue($groups_data));
+      ->will($this->returnValue($this->group_data));
 
     $result = $controller->run();
     $this->assertEquals($expected, $result);
